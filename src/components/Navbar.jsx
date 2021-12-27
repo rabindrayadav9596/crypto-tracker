ef7b38be2da7f55a8dfe1700b84ee5c348c2feae
Rabindra Yadav <rabindrayadav9596@gmail.com>
2021-12-26 20:37:32 -0600

    fixed build issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Menu, Typography, Avatar, Divider } from 'antd';
+import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
