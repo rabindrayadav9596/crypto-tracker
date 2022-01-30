56929f2ffa74d5a114152326a63f7ffda7d3d86f
Rabindra Yadav <rabindrayadav9596@gmail.com>
2022-01-30 10:17:34 -0600

    api v2 update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,9 +44,9 @@ const Navbar = () => {
         <Menu.Item icon={<FundOutlined />}>
           <Link to="/cryptocurrencies">Cryptocurrencies</Link>
         </Menu.Item>
-        <Menu.Item icon={<MoneyCollectOutlined />}>
+        {/* <Menu.Item icon={<MoneyCollectOutlined />}>
           <Link to="/exchanges">Exchanges</Link>
-        </Menu.Item>
+        </Menu.Item> */}
         
       </Menu>
       )}
